c86d166240e5f14e50d77832a49fb8a46564a5a3
TheLegend <amoghvarote@gmail.com>
2024-05-07 21:54:46 +0400

    Update proof for gichmich.py

--- proof for gichmich.py
+++ proof for gichmich.py
@@ -1,4 +1,38 @@
-def Print():
-    print("Gichmich is a dumbo")
-    
-Print()
+"""
+it is very easy you Gichelle dumb dumb
+
+see all you have to do is 3 steps:
+1) take input
+2) calculte
+3) print result
+
+simble :)
+"""
+
+
+# first you take the marks of the subjects
+meth = float(input("Enter marks for Math: "))
+Eunglish = float(input("Enter marks for English: "))
+eSESt = float(input("Enter marks for SST: "))
+Phyzics = float(input("Enter marks for Physics: "))
+Cimistry = float(input("Enter marks for Chemistry: "))
+
+# now you calculate total markk
+total_marks = meth+Eunglish+Phyzics+Cimistry
+
+# here calculate average
+average = total_marks / 5
+
+# and here you calculate percentage 
+percentage = (total_marks / 500) * 100
+
+# and you print here simblee
+print("Average marks:", average)
+print("Percentage:", percentage)
+
+# I like to add my own stuff in the code asw 😃😃
+
+if percentage < 99:
+    print("\nOH MY DAYSS YOU GOT LESS THAN 99% \n NAHH SKILL ISSUE FRFR :D\n")
+else:
+    print("OK NERD 🤓")
